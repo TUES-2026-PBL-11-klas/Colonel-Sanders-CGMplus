5a6009e5588f7d729a8ea41e06f90734e7931080
GogoManev <georgy.manev@gmail.com>
2026-03-18 16:38:26 +0200

    Added admin dashboard, - Currently nothing there as there aren't much features yet - Also changed some of the project's properties in app.json

--- CGMplus/app/(tabs)/profile.tsx
+++ CGMplus/app/(tabs)/profile.tsx
@@ -1,12 +1,21 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, Button } from 'react-native';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
+import { useRouter } from 'expo-router';
+
+
+let isAdmin = 1; //kato napravim userite i authentikaciqta shte go smenq
 
 export default function ExploreScreen() {
+  const router = useRouter();
   return (
+    <>
     <ThemedView style={styles.container}>
-      <ThemedText type="title">Profile</ThemedText>
+      <ThemedText type="title">Profile</ThemedText> 
     </ThemedView>
+    <ThemedText> {isAdmin ? 'You are an admin!' : 'Hello, user!'} </ThemedText>
+    {isAdmin && <Button title="Go to Admin Panel" onPress={() => router.push('/admin')} />}
+    </>
   );
 }
 
