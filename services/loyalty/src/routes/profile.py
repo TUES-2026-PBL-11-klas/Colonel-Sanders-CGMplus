116bcf01671c1c518e622243cf188f6929b39aaa
krasavecotklasa <cegrig4@gmail.com>
2026-04-15 17:28:49 +0300

    add: /card endpoint functionallity

--- services/loyalty/src/routes/profile.py
+++ services/loyalty/src/routes/profile.py
@@ -3,6 +3,8 @@ from flask.views import MethodView
 from src.schemas.profileSchema import ProfileSchema, CardSchema
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask import jsonify
+from src.models.card import Card as CardModel
+from uuid import UUID
 
 blp = Blueprint("profile", "profile", url_prefix="/profile")
 
@@ -24,6 +26,14 @@ class Profile(MethodView):
 
 @blp.route("/card")
 class Card(MethodView):
-    @blp.arguments(CardSchema)
+    @blp.response(200, CardSchema)
+    @blp.doc(security=[{"BearerAuth": []}])
+    @jwt_required()
     def get(self):
-        return jsonify({})
+        profile_id = get_jwt_identity()
+        card = CardModel.query.filter_by(id=UUID(profile_id)).first()
+
+        if not card:
+            return jsonify({'error': 'Card not found'}), 404
+
+        return jsonify({'nfc_id': card.nfc_id})
